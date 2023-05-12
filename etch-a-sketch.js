e1d5dfd28e18f90ad4f662916ca6ddfbc9017896
a-code-writer <124473111+a-code-writer@users.noreply.github.com>
2023-05-11 21:23:39 -0400

    started to make a 16x16 default grid, will add scaling code later, need to work on centering the grid, added code to make the container wrap, added max width and height, made the for loop work

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,15 +1,24 @@
 let div1 = document.createElement("div");
 document.body.style.display = 'flex';
 
-let divText = document.createTextNode("Hello");
-div1.style.maxWidth = '136px'
-div1.style.alignSelf = 'center'
+let divText = document.createTextNode("Hello"); 
 
 div1.appendChild(divText);  
 
 let container = document.getElementById('head_container')
+container.style.display = 'flex'
+container.style.flexWrap = 'wrap'
+container.style.maxHeight = '400px'
+container.style.maxWidth = '400px'
 
 console.log(container)  
 container.appendChild(divText);
 
+for (let i = 0; i < 50 ; i++) {
+    const div = document.createElement('div');
+    div.textContent = 'Hello'
+    div.id = `div_${i}`;
+    container.appendChild(div);
+
+  }
 
