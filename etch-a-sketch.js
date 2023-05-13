e041f3d13e3534dfbc3be377d8baeff13e358111
a-code-writer <124473111+a-code-writer@users.noreply.github.com>
2023-05-12 20:41:57 -0400

    made the grid a perfect square, added background color to the divs and borders, started adding an event handler for the 'hover' function. Moved all the styling over to the css sheet.

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,24 +1,16 @@
-let div1 = document.createElement("div");
-document.body.style.display = 'flex';
-
-let divText = document.createTextNode("Hello"); 
-
-div1.appendChild(divText);  
-
 let container = document.getElementById('head_container')
-container.style.display = 'flex'
-container.style.flexWrap = 'wrap'
-container.style.maxHeight = '400px'
-container.style.maxWidth = '400px'
 
-console.log(container)  
-container.appendChild(divText);
 
-for (let i = 0; i < 50 ; i++) {
+for (let i = 0; i < 64 ; i++) {
     const div = document.createElement('div');
     div.textContent = 'Hello'
     div.id = `div_${i}`;
+    div.className = 'squares';
+    div.addEventListener('hover', () => {
+      div.style.backgroundColor
+    }
+    )
     container.appendChild(div);
-
+    
   }
 
