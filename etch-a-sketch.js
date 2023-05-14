496f1433afdf786ad39d0320cf9e9158dd70bbad
a-code-writer <124473111+a-code-writer@users.noreply.github.com>
2023-05-13 20:20:39 -0400

    fixed the border issue, now the grid is a perfect sqaure and the borders do not interfere. Working on the reset button. Next is the scaling function.

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -3,14 +3,19 @@ let container = document.getElementById('head_container')
 
 for (let i = 0; i < 64 ; i++) {
     const div = document.createElement('div');
-    div.textContent = 'Hello'
+    //div.textContent = 'Hello'
     div.id = `div_${i}`;
     div.className = 'squares';
-    div.addEventListener('hover', () => {
-      div.style.backgroundColor
+    div.addEventListener('mouseover', () => {
+      div.style.backgroundColor = 'black';
     }
     )
     container.appendChild(div);
-    
   }
 
+  let resetBtn = document.getElementById('reset');
+  let square = document.getElementsByClassName('squares');
+  
+resetBtn.addEventListener('click', () => {
+  document.square.style.backgroundColor = '#8f8f8f';
+})
