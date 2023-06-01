2e18ba38b4befcd7c088ffd23809e5df6efd1f03
a-code-writer <124473111+a-code-writer@users.noreply.github.com>
2023-05-31 21:21:22 -0400

    found out why the reset button wasnt working. Had to convert the html collection that was returned by document.getElementsByClassName('sqaures') to a array and then pass that through a for loop to make every index (square) the color I wanted. Felt good to figure that out

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,9 +1,10 @@
+//establishes container div
 let container = document.getElementById('head_container')
 
 
+//creates the grid, gives each square it's own id
 for (let i = 0; i < 64 ; i++) {
     const div = document.createElement('div');
-    //div.textContent = 'Hello'
     div.id = `div_${i}`;
     div.className = 'squares';
     div.addEventListener('mouseover', () => {
@@ -13,9 +14,15 @@ for (let i = 0; i < 64 ; i++) {
     container.appendChild(div);
   }
 
-  let resetBtn = document.getElementById('reset');
-  let square = document.getElementsByClassName('squares');
-  
-resetBtn.addEventListener('click', () => {
-  document.square.style.backgroundColor = '#8f8f8f';
-})
+//establishes reset button
+let resetBtn = document.getElementById('reset');
+
+//selects the squares in the grid
+let square = document.getElementsByClassName('squares');
+
+//event listener for reset button not working right now.  
+resetBtn.addEventListener('mouseover', () => {
+  for (const square of document.getElementsByClassName('squares')) {
+    square.style.backgroundColor = '#8f8f8f';
+  }
+});
