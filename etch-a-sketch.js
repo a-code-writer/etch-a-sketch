c471d1ea3063da3781521b1886d9f402ee36d582
a-code-writer <124473111+a-code-writer@users.noreply.github.com>
2023-06-07 21:55:47 -0400

    the for loop works but I need to tweak the css to make the squares fit perfectly into the div no matter what the input is. Got the input to be converted into a variable that goes into the for loop.

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -3,7 +3,36 @@ let container = document.getElementById('head_container')
 
 
 //creates the grid, gives each square it's own id
-for (let i = 0; i < 64 ; i++) {
+// for (let i = 0; i < 64 ; i++) {
+//     const div = document.createElement('div');
+//     div.id = `div_${i}`;
+//     div.className = 'squares';
+//     div.addEventListener('mouseover', () => {
+//       div.style.backgroundColor = 'black';
+//     }
+//     )
+//     container.appendChild(div);
+//   }
+
+//establishes reset button
+let resetBtn = document.getElementById('reset');
+
+//selects the squares in the grid
+let square = document.getElementsByClassName('squares');
+
+//event listener for reset button not working right now.  
+resetBtn.addEventListener('click', () => {
+  for (const square of document.getElementsByClassName('squares')) {
+    square.style.backgroundColor = '#8f8f8f';
+  }
+});
+
+const scalingBtn = document.getElementById('scaling_Btn')
+
+function returnNum () {
+  let scalingInput = document.getElementById('scaling_Input').value;
+  alert(scalingInput)
+  for (let i = 0; i < scalingInput**2 ; i++) {
     const div = document.createElement('div');
     div.id = `div_${i}`;
     div.className = 'squares';
@@ -14,15 +43,9 @@ for (let i = 0; i < 64 ; i++) {
     container.appendChild(div);
   }
 
-//establishes reset button
-let resetBtn = document.getElementById('reset');
+}
 
-//selects the squares in the grid
-let square = document.getElementsByClassName('squares');
 
-//event listener for reset button not working right now.  
-resetBtn.addEventListener('mouseover', () => {
-  for (const square of document.getElementsByClassName('squares')) {
-    square.style.backgroundColor = '#8f8f8f';
-  }
-});
+scalingBtn.addEventListener('click', returnNum);
+
+
